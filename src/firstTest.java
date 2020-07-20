0195528cb5a02ea5eef60a1d8704d49f229d1410
Violet Evergarden <yss0917@qq.com>
2020-07-20 17:53:55 +0800

    some change

--- src/firstTest.java
+++ src/firstTest.java
@@ -2,7 +2,7 @@
 public class firstTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
+		System.out.println("First GitHub");
 
 	}
 	public static int add(int a, int b){
